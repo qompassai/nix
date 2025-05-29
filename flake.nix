4038ccee80bafe5821078ba3183c752da16a9778
phaedrusflow <map@qompass.ai>
2025-05-28 19:25:18 -0700

    Update metadata, badge, and citation for 2025-05-28

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 # /qompassai/nix/flake.nix
 # ------------------------------
 # Copyright (C) 2025 Qompass AI, All rights reserved
-
 {
   description = "Qompass AI Nix";
 
@@ -10,30 +9,34 @@
     nixpkgs.url = "https://flakehub.com/qompassai/NixOS/nixpkgs/*";
   };
 
-  outputs = { self, flake-schemas, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "aarch64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in {
-      nixosModules = {
-        languages = import ./modules/2/lang.nix;
-      };
-
-      schemas = flake-schemas.schemas;
-
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            curl
-            git
-            jq
-            wget
-            nixpkgs-fmt
-          ];
-        };
-      });
+  outputs = {
+    self,
+    flake-schemas,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-darwin" "aarch64-linux"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    nixosModules = {
+      languages = import ./modules/2/lang.nix;
     };
-}
 
+    schemas = flake-schemas.schemas;
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          curl
+          git
+          jq
+          wget
+          nixpkgs-fmt
+        ];
+      };
+    });
+  };
+}
